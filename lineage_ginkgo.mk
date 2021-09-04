e6dac0155cd676dc6b8f46ad4cafc2cf9c2946aa
Ashwin.D.S <astroashwin@outlook.com>
2021-09-04 08:42:12 +0530

    ginkgo: update maintainer flag

--- lineage_ginkgo.mk
+++ lineage_ginkgo.mk
@@ -30,7 +30,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Sakura Flags
 TARGET_BOOT_ANIMATION_RES := 1080
-SAKURA_MAINTAINER := Ashwin&Karan
+SAKURA_MAINTAINER := Ashwin
 TARGET_GAPPS_ARCH := arm64
 SAKURA_BUILD_TYPE=basicgapps
 
